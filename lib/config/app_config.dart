941f119d7cb6d49724f39060ab407b495a26ccbe
Jhandry Becerra <tu_correo@ejemplo.com>
2026-06-03 18:57:06 -0500

    add dio and go_router packages and basic config

---
+++ lib/config/app_config.dart
@@ -0,0 +1,7 @@
+class AppConfig {
+  static const String apiBaseUrl =
+      'https://6a1cf27ebcc4f20d5ca3b7bc.mockapi.io/api/v1';
+
+  static const Duration connectTimeout = Duration(seconds: 15);
+  static const Duration receiveTimeout = Duration(seconds: 20);
+}
